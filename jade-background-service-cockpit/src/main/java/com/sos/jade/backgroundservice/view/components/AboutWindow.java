c1f7928be1729217d984bfefce8ccd309fdfe267
Santiago Aucejo Petzoldt <santiago.aucejo.petzoldt.git@sos-berlin.com>
2014-07-22 08:51:50 +0000

    Solution(workaround) for the db access problem: validation-api set to none in hibernate configuration file This Problem stems from the update of the hibernate jars from version 2.x to 3.x see http://stackoverflow.com/questions/7658730/hibernateexception-unable-to-get-the-default-bean-validation-factory
    svn-id 26465

--- jade-background-service-cockpit/src/main/java/com/sos/jade/backgroundservice/view/components/AboutWindow.java
+++ jade-background-service-cockpit/src/main/java/com/sos/jade/backgroundservice/view/components/AboutWindow.java
@@ -5,14 +5,15 @@ import com.vaadin.ui.Window;
 
 public class AboutWindow extends Window {
 	private static final long serialVersionUID = 1L;
-
+	private static final int WIDTH = 400;
+	private static final float WIDTH_FL = 400.0f;
 	public AboutWindow() {
 		setModal(false);
 		setDraggable(false);
 		setResizable(false);
 		setHeight(160.0f, Unit.PIXELS);
-		setWidth(400.0f, Unit.PIXELS);
-		setPositionX(Page.getCurrent().getBrowserWindowWidth() - 400);
+		setWidth(WIDTH_FL, Unit.PIXELS);
+		setPositionX(Page.getCurrent().getBrowserWindowWidth() - WIDTH);
 		setPositionY(0);
 //		addStyleName("jadeAboutWindow");
 		setContent(new AboutLayout());
