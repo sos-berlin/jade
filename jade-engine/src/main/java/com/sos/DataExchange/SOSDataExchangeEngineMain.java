0cec62114315da9eee9b41f4a0c1f0b675e074a4
Klaus Büttner <klaus.buettner.git@sos-berlin.com>
2014-09-10 18:43:04 +0000

    -
    svn-id 27146

--- jade-engine/src/main/java/com/sos/DataExchange/SOSDataExchangeEngineMain.java
+++ jade-engine/src/main/java/com/sos/DataExchange/SOSDataExchangeEngineMain.java
@@ -27,9 +27,7 @@ import com.sos.i18n.annotation.I18NResourceBundle;
  * mechanicaly created by C:\Users\KB\eclipse\xsl\JSJobDoc2JSMainClass.xsl from http://www.sos-berlin.com at 20100930175655
  * \endverbatim
  */
-@I18NResourceBundle(
-					baseName = "SOSDataExchange",
-					defaultLocale = "en")
+@I18NResourceBundle(baseName = "SOSDataExchange", defaultLocale = "en")
 public class SOSDataExchangeEngineMain extends I18NBase implements JSJobUtilities {
 	private final static String		conClassName	= "SOSDataExchangeEngineMain";
 	public static final String		conSVNVersion	= "$Id$";
@@ -75,7 +73,9 @@ public class SOSDataExchangeEngineMain extends I18NBase implements JSJobUtilitie
 			objO.ApplicationName.Value(new SOSMsgJade("SOSJADE_T_0020").get());
 			objO.ApplicationDocuUrl.Value(new SOSMsgJade("SOSJADE_T_0021").get());
 			objO.AllowEmptyParameterList.setFalse();
-			@SuppressWarnings("unused") String strLog4jPropertyFileName = objO.log4jPropertyFileName.Value();
+			objO.CheckNotProcessedOptions.setTrue();
+			@SuppressWarnings("unused")
+			String strLog4jPropertyFileName = objO.log4jPropertyFileName.Value();
 			for (String strParam : pstrArgs) {
 				if (strParam.toLowerCase().startsWith("-log4jPropertyFileName")) {
 					String[] strS = strParam.split("=");
@@ -92,7 +92,20 @@ public class SOSDataExchangeEngineMain extends I18NBase implements JSJobUtilitie
 			logger.info(getMsg(SOSDX_Intro) + " -- " + VersionInfo.VERSION_STRING);
 			logger.debug(conSVNVersion);
 			objO.CheckMandatory();
+
+			objO.CommandLineArgs(pstrArgs);
+			if (objO.CheckNotProcessedOptions.isTrue()) {
+				if (objO.CheckNotProcessedOptions() == true) {
+				}
+				else {
+					JADEException objE = new JADEException("Unsupported or wrong Options found.");
+					objE.setExitCode(JADEExitCodes.UnsupportedParametersFound);
+					throw objE;
+				}
+			}
+
 			boolean flgResult = objM.Execute();
+
 			objM.Logout();
 			if (flgResult == false || objM.flgGlobalError == true) {
 				setExitCode(99);
@@ -108,7 +121,8 @@ public class SOSDataExchangeEngineMain extends I18NBase implements JSJobUtilitie
 		logger.info(String.format(getMsg(SOS_EXIT_WO_ERRORS), conMethodName));
 	} // private void Execute
 
-	@SuppressWarnings("unused") private void setExitCode() {
+	@SuppressWarnings("unused")
+	private void setExitCode() {
 		setExitCode(99);
 	}
 
@@ -127,114 +141,95 @@ public class SOSDataExchangeEngineMain extends I18NBase implements JSJobUtilitie
 			logger.info("exit with code = " + intExitCode);
 		}
 	}
-	@I18NMessages(
-					value = { @I18NMessage("SOSDataExchange - Main routine started ..."), //
-			@I18NMessage(
-							value = "SOSDataExchange - Main",
-							locale = "en_UK", //
-							explanation = "SOSDataExchange - Main" //
+	@I18NMessages(value = { @I18NMessage("SOSDataExchange - Main routine started ..."), //
+			@I18NMessage(value = "SOSDataExchange - Main", locale = "en_UK", //
+			explanation = "SOSDataExchange - Main" //
 			), //
-			@I18NMessage(
-							value = "SOSDataExchange - Kommandozeilenprogram startet ....",
-							locale = "de", //
-							explanation = "SOSDataExchange - Main" //
-			)		//
-					},
-					msgnum = "SOSDX-I-9999",
-					msgurl = "")
+			@I18NMessage(value = "SOSDataExchange - Kommandozeilenprogram startet ....", locale = "de", //
+			explanation = "SOSDataExchange - Main" //
+			) //
+	}, msgnum = "SOSDX-I-9999", msgurl = "")
 	/*!
 	 * \var SOSDX-Intro
 	 * \brief SOSDataExchange - Main
 	 */
 	public static final String	SOSDX_Intro				= "SOSDataExchangeEngineMain.SOSDX-Intro";
-	@I18NMessages(
-					value = { @I18NMessage("%1$s: Error occurred ...: %2$s, exit-code 99 raised"), //
-			@I18NMessage(
-							value = "%1$s: Error occurred ...: %2$s",
-							locale = "en_UK", //
-							explanation = "%1$s: Error occurred ...: %2$s" //
+	@I18NMessages(value = { @I18NMessage("%1$s: Error occurred ...: %2$s, exit-code 99 raised"), //
+			@I18NMessage(value = "%1$s: Error occurred ...: %2$s", locale = "en_UK", //
+			explanation = "%1$s: Error occurred ...: %2$s" //
 			), //
-			@I18NMessage(
-							value = "%1$s: Fehler aufgetreten: %2$s, Programm wird mit Exit-Code 99 beendet.",
-							locale = "de", //
-							explanation = "%1$s: Error occurred ...: %2$s" //
-			)		//
-					},
-					msgnum = "SOSDX-E-0001",
-					msgurl = "")
+			@I18NMessage(value = "%1$s: Fehler aufgetreten: %2$s, Programm wird mit Exit-Code 99 beendet.", locale = "de", //
+			explanation = "%1$s: Error occurred ...: %2$s" //
+			) //
+	}, msgnum = "SOSDX-E-0001", msgurl = "")
 	/*!
 	 * \var SOSDX_E_0001
 	 * \brief %1$s: Error occurred ...: %2$s
 	 */
 	public static final String	SOSDX_E_0001			= "SOSDataExchangeEngineMain.SOSDX_E_0001";
-	@I18NMessages(
-					value = { @I18NMessage("%1$s - ended without errorsended without errors"), //
-			@I18NMessage(
-							value = "%1$s - ended without errors",
-							locale = "en_UK", //
-							explanation = "%1$s - ended without errorsended without errors" //
+	@I18NMessages(value = { @I18NMessage("%1$s - ended without errorsended without errors"), //
+			@I18NMessage(value = "%1$s - ended without errors", locale = "en_UK", //
+			explanation = "%1$s - ended without errorsended without errors" //
 			), //
-			@I18NMessage(
-							value = "%1$s - Programm wurde ohne Fehler beendet",
-							locale = "de", //
-							explanation = "%1$s - ended without errorsended without errors" //
-			)		//
-					},
-					msgnum = "SOS-I-106",
-					msgurl = "")
+			@I18NMessage(value = "%1$s - Programm wurde ohne Fehler beendet", locale = "de", //
+			explanation = "%1$s - ended without errorsended without errors" //
+			) //
+	}, msgnum = "SOS-I-106", msgurl = "")
 	/*!
 	 * \var SOS_EXIT_WO_ERRORS
 	 * \brief %1$s - ended without errorsended without errors
 	 */
 	public static final String	SOS_EXIT_WO_ERRORS		= "SOSDataExchangeEngineMain.SOS_EXIT_WO_ERRORS";
-	@I18NMessages(
-					value = { @I18NMessage("%1$s - terminated with exit-code %2$d"), //
-			@I18NMessage(
-							value = "%1$s - terminated with exit-code %2$d",
-							locale = "en_UK", //
-							explanation = "%1$s - terminated with exit-code %2$d" //
+	@I18NMessages(value = { @I18NMessage("%1$s - terminated with exit-code %2$d"), //
+			@I18NMessage(value = "%1$s - terminated with exit-code %2$d", locale = "en_UK", //
+			explanation = "%1$s - terminated with exit-code %2$d" //
 			), //
-			@I18NMessage(
-							value = "%1$s - Fehlercode %2$d wurde gesetzt",
-							locale = "de", //
-							explanation = "%1$s - terminated with exit-code %2$d" //
-			)		//
-					},
-					msgnum = "SOSDX_E_0002",
-					msgurl = "")
+			@I18NMessage(value = "%1$s - Fehlercode %2$d wurde gesetzt", locale = "de", //
+			explanation = "%1$s - terminated with exit-code %2$d" //
+			) //
+	}, msgnum = "SOSDX_E_0002", msgurl = "")
 	/*!
 	 * \var SOS_EXIT_CODE_RAISED
 	 * \brief %1$s - terminated with exit-code %2$d
 	 */
 	public static final String	SOS_EXIT_CODE_RAISED	= "SOSDataExchangeEngineMain.SOS_EXIT_CODE_RAISED";
 
-	@Override public String myReplaceAll(final String source, final String what, final String replacement) {
+	@Override
+	public String myReplaceAll(final String source, final String what, final String replacement) {
 		return source;
 	}
 
-	@Override public String replaceSchedulerVars(final boolean isWindows, final String pstrString2Modify) {
+	@Override
+	public String replaceSchedulerVars(final boolean isWindows, final String pstrString2Modify) {
 		return pstrString2Modify;
 	}
 
-	@Override public void setJSParam(final String pstrKey, final String pstrValue) {
+	@Override
+	public void setJSParam(final String pstrKey, final String pstrValue) {
 	}
 
-	@Override public void setJSParam(final String pstrKey, final StringBuffer pstrValue) {
+	@Override
+	public void setJSParam(final String pstrKey, final StringBuffer pstrValue) {
 	}
 
-	@Override public String getCurrentNodeName() {
+	@Override
+	public String getCurrentNodeName() {
 		return "";
 	}
 
-	@Override public void setJSJobUtilites(final JSJobUtilities pobjJSJobUtilities) {
+	@Override
+	public void setJSJobUtilites(final JSJobUtilities pobjJSJobUtilities) {
 	}
 
-	@Override public void setStateText(final String pstrStateText) {
+	@Override
+	public void setStateText(final String pstrStateText) {
 	}
 
-	@Override public void setCC(final int pintCC) {
+	@Override
+	public void setCC(final int pintCC) {
 	}
 
-	@Override public void setNextNodeState(final String pstrNodeName) {
+	@Override
+	public void setNextNodeState(final String pstrNodeName) {
 	}
 } // class SOSDataExchangeEngineMain
