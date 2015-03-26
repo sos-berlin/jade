6267ea7531159e1f6fd59f9697a0c9eae8aa9cf2
SOS-RE <robert.ehrlich@sos-berlin.com>
2015-03-26 12:53:03 +0100

    JADE-274 (Tests SFTP)

---
+++ jade-engine/src/test/java/com/sos/DataExchange/JadeTestsSFTP.java
@@ -0,0 +1,66 @@
+package com.sos.DataExchange;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sos.DataExchange.Options.JADEOptions;
+
+
+public class JadeTestsSFTP extends JadeTestBase{
+	
+	private JADEOptions options;
+	
+	public JadeTestsSFTP() {
+	}
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		options = new JADEOptions();
+		options.settings.Value("src/test/resources/examples/jade_sftp_settings.ini");
+	}
+
+	@After
+	public void tearDown() throws Exception{
+			
+	}
+
+	@Test
+	public void testLocal2sftp() throws Exception {
+		options.profile.Value("local_2_sftp");
+		
+		this.execute(options);
+	}
+
+	@Test
+	public void testHttpProxyLocal2sftp() throws Exception {
+		options.profile.Value("http_proxy_local_2_sftp");
+		
+		this.execute(options);
+	}
+	
+	@Test
+	public void testSocks5ProxyLocal2sftp() throws Exception {
+		options.profile.Value("socks5_proxy_local_2_sftp");
+		
+		this.execute(options);
+	}
+	
+	private void execute(JADEOptions options) throws Exception{
+		try{
+			objJadeEngine = new JadeEngine(options);
+			objJadeEngine.Execute();
+		}
+		catch(Exception ex){
+			throw ex;
+		}
+		finally{
+			objJadeEngine.Logout();	
+		}
+	}
+
+}
