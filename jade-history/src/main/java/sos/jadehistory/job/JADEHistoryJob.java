a4e2e1efde4408229ba2915d390f8653d7549fa4
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2015-10-15 20:16:07 +0200

    JADE-377

--- jade-history/src/main/java/sos/jadehistory/job/JADEHistoryJob.java
+++ jade-history/src/main/java/sos/jadehistory/job/JADEHistoryJob.java
@@ -777,9 +777,10 @@ public class JADEHistoryJob extends JobSchedulerJobAdapter {
 			}
 		} else if (mappingName.equals("mapping_source_dir")) {
 			len = 255;
-			if (attr_val.length() == 0) {
+			//is possibly empty for file path selection
+			/* if (attr_val.length() == 0) {
 				throw new JobSchedulerException("parameter [" + attr_name + "] can't be empty");
-			}
+			} */
 			if (operation != null && operation.equals(_operationReceive)) {
 				attr_val = getRecordValue(record, "mapping_target_dir", _operationSend);
 			}
