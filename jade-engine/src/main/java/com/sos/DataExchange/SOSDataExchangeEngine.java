0973d090407e25931148b0a60aa111a03dfa0083
Oliver Haufe <oliver.haufe@sos-berlin.com>
2017-02-08 15:51:49 +0100

    YADE-420

--- jade-engine/src/main/java/com/sos/DataExchange/SOSDataExchangeEngine.java
+++ jade-engine/src/main/java/com/sos/DataExchange/SOSDataExchangeEngine.java
@@ -810,10 +810,9 @@ public class SOSDataExchangeEngine extends JadeBaseEngine implements Runnable, I
     }
 
     private void sendNotifications() {
-        SOSSmtpMailOptions mailOptions = objOptions.getMailOptions();
         if (objOptions.mailOnError.isTrue() && (sourceFileList.getFailedTransfers() > 0 || JobSchedulerException.LastErrorMessage.length() > 0)) {
             doProcessMail(enuMailClasses.MailOnError);
-        } else if (objOptions.mail_on_success.isTrue() && sourceFileList.FailedTransfers() <= 0) {
+        } else if (objOptions.mailOnSuccess.isTrue() && sourceFileList.getFailedTransfers() <= 0) {
             doProcessMail(enuMailClasses.MailOnSuccess);
         }
         if (objOptions.mailOnEmptyFiles.isTrue() && sourceFileList.getZeroByteCount() > 0) {
