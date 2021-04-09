402a8c5a728bbee3765c1c490b021468e5d0421e
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-04-09 14:48:00 +0200

    YADE-573

--- jade-db-utils/src/main/java/com/sos/jade/db/DBItemYadeFiles.java
+++ jade-db-utils/src/main/java/com/sos/jade/db/DBItemYadeFiles.java
@@ -12,6 +12,7 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
@@ -40,7 +41,7 @@ public class DBItemYadeFiles extends DbItem implements Serializable {
     private String errorMessage;
     private String integrityHash;
     private Date modified;
-    
+
     public DBItemYadeFiles() {
     }
 
@@ -51,7 +52,7 @@ public class DBItemYadeFiles extends DbItem implements Serializable {
     public Long getId() {
         return id;
     }
-    
+
     /** Primary key */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = YADE_FILES_SEQUENCE)
@@ -59,104 +60,109 @@ public class DBItemYadeFiles extends DbItem implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     @Column(name = "[TRANSFER_ID]", nullable = false)
     public Long getTransferId() {
         return transferId;
     }
-    
+
     @Column(name = "[TRANSFER_ID]", nullable = false)
     public void setTransferId(Long transferId) {
         this.transferId = transferId;
     }
-    
+
     @Column(name = "[INTERVENTION_TRANSFER_ID]", nullable = true)
     public Long getInterventionTransferId() {
         return interventionTransferId;
     }
-    
+
     @Column(name = "[INTERVENTION_TRANSFER_ID]", nullable = true)
     public void setInterventionTransferId(Long interventionTransferId) {
         this.interventionTransferId = interventionTransferId;
     }
-    
+
     @Column(name = "[SOURCE_PATH]", nullable = false)
     public String getSourcePath() {
         return sourcePath;
     }
-    
+
     @Column(name = "[SOURCE_PATH]", nullable = false)
     public void setSourcePath(String sourcePath) {
         this.sourcePath = sourcePath;
     }
-    
+
     @Column(name = "[TARGET_PATH]", nullable = true)
     public String getTargetPath() {
         return targetPath;
     }
-    
+
     @Column(name = "[TARGET_PATH]", nullable = true)
     public void setTargetPath(String targetPath) {
         this.targetPath = targetPath;
     }
-    
+
     @Column(name = "[SIZE]", nullable = true)
     public Long getSize() {
         return size;
     }
-    
+
     @Column(name = "[SIZE]", nullable = true)
     public void setSize(Long size) {
         this.size = size;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[MODIFICATION_DATE]", nullable = true)
     public Date getModificationDate() {
         return modificationDate;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[MODIFICATION_DATE]", nullable = true)
     public void setModificationDate(Date modificationDate) {
         this.modificationDate = modificationDate;
     }
-    
+
     @Column(name = "[STATE]", nullable = false)
     public Integer getState() {
         return state;
     }
-    
+
     @Column(name = "[STATE]", nullable = false)
     public void setState(Integer state) {
         this.state = state;
     }
-    
+
     @Column(name = "[ERROR_CODE]", nullable = true)
     public String getErrorCode() {
         return errorCode;
     }
-    
+
     @Column(name = "[ERROR_CODE]", nullable = true)
     public void setErrorCode(String errorCode) {
         this.errorCode = errorCode;
     }
-    
+
     @Column(name = "[ERROR_MESSAGE]", nullable = true)
     public String getErrorMessage() {
         return errorMessage;
     }
-    
+
     @Column(name = "[ERROR_MESSAGE]", nullable = true)
     public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
+        this.errorMessage = normalizeErrorMessage(errorMessage);
     }
-    
+
+    @Transient
+    public static String normalizeErrorMessage(String val) {
+        return normalizeValue(val, 4000);
+    }
+
     @Column(name = "[INTEGRITY_HASH]", nullable = true)
     public String getIntegrityHash() {
         return integrityHash;
     }
-    
+
     @Column(name = "[INTEGRITY_HASH]", nullable = true)
     public void setIntegrityHash(String integrityHash) {
         this.integrityHash = integrityHash;
@@ -167,13 +173,13 @@ public class DBItemYadeFiles extends DbItem implements Serializable {
     public Date getModified() {
         return modified;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[MODIFIED]", nullable = false)
     public void setModified(Date modified) {
         this.modified = modified;
     }
-    
+
     @Override
     public int hashCode() {
         // always build on unique constraint
--- jade-db-utils/src/main/java/com/sos/jade/db/DBItemYadeTransfers.java
+++ jade-db-utils/src/main/java/com/sos/jade/db/DBItemYadeTransfers.java
@@ -12,6 +12,7 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
@@ -51,10 +52,10 @@ public class DBItemYadeTransfers extends DbItem implements Serializable {
     private Long numOfFiles;
     private String profileName;
     private Date modified;
-    
+
     public DBItemYadeTransfers() {
     }
-    
+
     /** Primary key */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = YADE_TRANSFERS_SEQUENCE)
@@ -62,7 +63,7 @@ public class DBItemYadeTransfers extends DbItem implements Serializable {
     public Long getId() {
         return id;
     }
-    
+
     /** Primary key */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = YADE_TRANSFERS_SEQUENCE)
@@ -70,146 +71,151 @@ public class DBItemYadeTransfers extends DbItem implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     @Column(name = "[SOURCE_PROTOCOL_ID]", nullable = false)
     public Long getSourceProtocolId() {
         return sourceProtocolId;
     }
-    
+
     @Column(name = "[SOURCE_PROTOCOL_ID]", nullable = false)
     public void setSourceProtocolId(Long sourceProtocolId) {
         this.sourceProtocolId = sourceProtocolId;
     }
-    
+
     @Column(name = "[TARGET_PROTOCOL_ID]", nullable = true)
     public Long getTargetProtocolId() {
         return targetProtocolId;
     }
-    
+
     @Column(name = "[TARGET_PROTOCOL_ID]", nullable = true)
     public void setTargetProtocolId(Long targetProtocolId) {
         this.targetProtocolId = targetProtocolId;
     }
-    
+
     @Column(name = "[JUMP_PROTOCOL_ID]", nullable = true)
     public Long getJumpProtocolId() {
         return jumpProtocolId;
     }
-    
+
     @Column(name = "[JUMP_PROTOCOL_ID]", nullable = true)
     public void setJumpProtocolId(Long jumpProtocolId) {
         this.jumpProtocolId = jumpProtocolId;
     }
-    
+
     @Column(name = "[MANDATOR]", nullable = true)
     public String getMandator() {
         return mandator;
     }
-    
+
     @Column(name = "[MANDATOR]", nullable = true)
     public void setMandator(String mandator) {
         this.mandator = mandator;
     }
-    
+
     @Column(name = "[OPERATION]", nullable = false)
     public Integer getOperation() {
         return operation;
     }
-    
+
     @Column(name = "[OPERATION]", nullable = false)
     public void setOperation(Integer operation) {
         this.operation = operation;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[START]", nullable = false)
     public Date getStart() {
         return start;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[START]", nullable = false)
     public void setStart(Date start) {
         this.start = start;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[END]", nullable = true)
     public Date getEnd() {
         return end;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[END]", nullable = true)
     public void setEnd(Date end) {
         this.end = end;
     }
-    
+
     @Column(name = "[STATE]", nullable = false)
     public Integer getState() {
         return state;
     }
-    
+
     @Column(name = "[STATE]", nullable = false)
     public void setState(Integer state) {
         this.state = state;
     }
-    
+
     @Column(name = "[ERROR_CODE]", nullable = true)
     public String getErrorCode() {
         return errorCode;
     }
-    
+
     @Column(name = "[ERROR_CODE]", nullable = true)
     public void setErrorCode(String errorCode) {
         this.errorCode = errorCode;
     }
-    
+
     @Column(name = "[ERROR_MESSAGE]", nullable = true)
     public String getErrorMessage() {
         return errorMessage;
     }
-    
+
     @Column(name = "[ERROR_MESSAGE]", nullable = true)
     public void setErrorMessage(String errorMessage) {
-        this.errorMessage = errorMessage;
+        this.errorMessage = normalizeErrorMessage(errorMessage);
     }
-    
+
+    @Transient
+    public static String normalizeErrorMessage(String val) {
+        return normalizeValue(val, 4000);
+    }
+
     @Column(name = "[JOBSCHEDULER_ID]", nullable = true)
     public String getJobschedulerId() {
         return jobschedulerId;
     }
-    
+
     @Column(name = "[JOBSCHEDULER_ID]", nullable = true)
     public void setJobschedulerId(String jobschedulerId) {
         this.jobschedulerId = jobschedulerId;
     }
-    
+
     @Column(name = "[JOB]", nullable = true)
     public String getJob() {
         return job;
     }
-    
+
     @Column(name = "[JOB]", nullable = true)
     public void setJob(String job) {
         this.job = job;
     }
-    
+
     @Column(name = "[JOB_CHAIN]", nullable = true)
     public String getJobChain() {
         return jobChain;
     }
-    
+
     @Column(name = "[JOB_CHAIN]", nullable = true)
     public void setJobChain(String jobChain) {
         this.jobChain = jobChain;
     }
-    
+
     @Column(name = "[JOB_CHAIN_NODE]", nullable = true)
     public String getJobChainNode() {
         return jobChainNode;
     }
-    
+
     @Column(name = "[JOB_CHAIN_NODE]", nullable = true)
     public void setJobChainNode(String jobChainNode) {
         this.jobChainNode = jobChainNode;
@@ -219,17 +225,17 @@ public class DBItemYadeTransfers extends DbItem implements Serializable {
     public String getOrderId() {
         return orderId;
     }
-    
+
     @Column(name = "[ORDER_ID]", nullable = true)
     public void setOrderId(String orderId) {
         this.orderId = orderId;
     }
-    
+
     @Column(name = "[TASK_ID]", nullable = false)
     public Long getTaskId() {
         return taskId;
     }
-    
+
     @Column(name = "[TASK_ID]", nullable = false)
     public void setTaskId(Long taskId) {
         this.taskId = taskId;
@@ -239,66 +245,66 @@ public class DBItemYadeTransfers extends DbItem implements Serializable {
     public Long getAuditLogId() {
         return auditLogId;
     }
-    
+
     @Column(name = "[AUDIT_LOG_ID]", nullable = true)
     public void setAuditLogId(Long auditLogId) {
         this.auditLogId = auditLogId;
     }
-    
+
     @Column(name = "[HAS_INTERVENTION]", nullable = true)
     @Type(type = "numeric_boolean")
     public Boolean getHasIntervention() {
         return hasIntervention;
     }
-    
+
     @Column(name = "[HAS_INTERVENTION]", nullable = true)
     @Type(type = "numeric_boolean")
     public void setHasIntervention(Boolean hasIntervention) {
         this.hasIntervention = hasIntervention;
     }
-    
+
     @Column(name = "[PARENT_TRANSFER_ID]", nullable = true)
     public Long getParentTransferId() {
         return parentTransferId;
     }
-    
+
     @Column(name = "[PARENT_TRANSFER_ID]", nullable = true)
     public void setParentTransferId(Long parentTransferId) {
         this.parentTransferId = parentTransferId;
     }
-    
+
     @Column(name = "[NUM_OF_FILES]", nullable = true)
     public Long getNumOfFiles() {
         return numOfFiles;
     }
-    
+
     @Column(name = "[NUM_OF_FILES]", nullable = true)
     public void setNumOfFiles(Long numOfFiles) {
         this.numOfFiles = numOfFiles;
     }
-    
+
     @Column(name = "[PROFILE_NAME]", nullable = true)
     public String getProfileName() {
         return profileName;
     }
-    
+
     @Column(name = "[PROFILE_NAME]", nullable = true)
     public void setProfileName(String profileName) {
         this.profileName = profileName;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[MODIFIED]", nullable = false)
     public Date getModified() {
         return modified;
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "[MODIFIED]", nullable = false)
     public void setModified(Date modified) {
         this.modified = modified;
     }
-    
+
     @Override
     public int hashCode() {
         // always build on unique constraint or, if not exists, primary key (unique)
