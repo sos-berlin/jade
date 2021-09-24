bfdfba2a9a3b61f88b5ac2b51f38eb2b861851c7
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-09-24 16:48:16 +0200

    YADE-577-sshj

--- jade-engine/src/test/java/com/sos/DataExchange/JadeEngineTest.java
+++ jade-engine/src/test/java/com/sos/DataExchange/JadeEngineTest.java
@@ -5,12 +5,14 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import com.sos.vfs.common.options.SOSBaseOptions;
+import com.sos.vfs.common.options.SOSProviderOptions;
+import com.sos.vfs.sftp.SOSSFTP;
 import com.sos.vfs.sftp.SOSSFTP.SSHProvider;
 
 public class JadeEngineTest {
 
-    private static final String SETTINGS_FILE = "R:/backup/sos/java/development/SOSDataExchange/examples/jade_sftp_settings.xml";
-    private static final String PROFILE = "copy_sftp_localhost_2_local";
+    private static final String SETTINGS_FILE = "D:/Temp/sftp.xml";
+    private static final String PROFILE = "copy_sftp_galadriel_2_local";
     private SOSBaseOptions options;
 
     public JadeEngineTest() {
@@ -28,6 +30,33 @@ public class JadeEngineTest {
         options.ssh_provider.setValue(SSHProvider.SSHJ.name());
         options.profile.setValue(PROFILE);
         this.execute(options);
+    }
+
+    @Ignore
+    @Test
+    public void testSSHProvider() throws Exception {
+        options.ssh_provider.setValue(SSHProvider.SSHJ.name());
+        options.profile.setValue(PROFILE);
+
+        SOSDataExchangeEngine engine = new SOSDataExchangeEngine(options);
+        SOSProviderOptions pOptions = engine.getOptions().getSource();
+        // pOptions.useZlibCompression.setValue("true");
+
+        SOSSFTP p = new SOSSFTP(options.ssh_provider);
+        try {
+            p.connect(pOptions);
+
+            String source = "D:/Temp/my.exe";
+            String target = "/home/sos/my.exe";
+
+            p.putFile(source, target);
+            p.get(target, source + ".get.exe");
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            p.disconnect();
+        }
     }
 
     @Ignore
